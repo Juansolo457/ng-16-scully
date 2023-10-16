8c156a3a90b829603f27868c10c10233f256fc7e
Juansolo457 <isitluk@gmail.com>
2023-10-15 21:09:19 -0700

    added scully and config for blog posts, WIP

--- my-blog/src/app/app.module.ts
+++ my-blog/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ContactModule } from './contact/contact.module';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { ScullyLibModule} from '@scullyio/ng-lib'
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    SharedModule,
+    ContactModule,
+    ScullyLibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
